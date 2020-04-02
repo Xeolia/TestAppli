0d806a6d3483b787f9ce34b031ee1f07054491fb
Xeolia <alexia.serrierb@gmail.com>
2020-04-02 18:39:07 +0200

    message

---
+++ app/src/main/java/com/example/applicationtest/SecondFragment.java
@@ -0,0 +1,41 @@
+package com.example.applicationtest;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class SecondFragment extends Fragment{
+    public Button button;
+
+    public void init(){
+        //button= (Button)findViewById;
+    }
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
+
+
