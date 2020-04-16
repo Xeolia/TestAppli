53d7b6fab19c3f38fb3b1869855af11c34d6f223
Xeolia <alexia.serrierb@gmail.com>
2020-04-16 11:54:54 +0200

    Bug sur boutons

--- app/src/main/java/com/example/applicationtest/MainActivity.java
+++ app/src/main/java/com/example/applicationtest/MainActivity.java
@@ -7,25 +7,46 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button button;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        this.button= findViewById(R.id.button);
-
+        setContentView(R.layout.fragment_second);
+        Button one = (Button) findViewById(R.id.button);
+        one.setOnClickListener(this);
+       // this.button= findViewById(R.id.button);
+        Button two = (Button) findViewById(R.id.button2);
+        two.setOnClickListener(this);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),MyNewActivity.class);
-                startActivity(intent);
-                //finish();
 
+                //finish();
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.button:
+                // do your code
+                Intent intent = new Intent(getApplicationContext(),SecondFragment.class);
+                startActivity(intent);
+                break;
+
+            case R.id.button2:
+                // do your code
+                Intent intent2 = new Intent(getApplicationContext(),ThirdFragment.class);
+                startActivity(intent2);
+                break;
+
+            default:
+                System.out.println("Une erreur s'est produit ");
+                break;
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/applicationtest/ThirdFragment.java
@@ -0,0 +1,16 @@
+package com.example.applicationtest;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ThirdFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_third);
+
+    }
+}
+
