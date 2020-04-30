7a26b9dfcc66f5eff0d6fd0120d60948de10443d
Xeolia <alexia.serrierb@gmail.com>
2020-04-30 09:47:15 +0200

    Delete de la page et essais boutons

--- app/src/main/java/com/example/applicationtest/MainActivity.java
+++ app/src/main/java/com/example/applicationtest/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.applicationtest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,25 +15,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
     }
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
+        public void onClick(View v) {
+            switch (v.getId()) {
 
-            case R.id.button:
-                // do your code
-                Intent intent = new Intent(getApplicationContext(),SecondFragment.class);
-                startActivity(intent);
-                break;
+                case R.id.button:
+                    // do your code
+                    Intent intent = new Intent(getApplicationContext(), SecondFragment.class);
+                    startActivity(intent);
+                    break;
 
-            case R.id.button2:
-                // do your code
-                Intent intent2 = new Intent(getApplicationContext(),ThirdFragment.class);
-                startActivity(intent2);
-                break;
+                case R.id.buttonSecondeCarte:
+                    // do your code
+                    Intent intent2 = new Intent(getApplicationContext(), ThirdFragment.class);
+                    startActivity(intent2);
+                    break;
 
-            default:
-                System.out.println("Une erreur s'est produit ");
-                break;
+                default:
+                    System.out.println("Une erreur s'est produit ");
+                    break;
+            }
+
         }
-
+    private void showError() {
+        Toast.makeText(getApplicationContext(),"Error",Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/applicationtest/ThirdFragment.java
+++ app/src/main/java/com/example/applicationtest/ThirdFragment.java
@@ -1,15 +1,26 @@
 package com.example.applicationtest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ThirdFragment extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_third);
+        Button one = (Button) findViewById(R.id.buttonSecondeCarte);
+
+        one.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent intent = new Intent(ThirdFragment.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
